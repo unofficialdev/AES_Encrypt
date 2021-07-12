a4aff3022cea73567982f92210bbf0efbb69f082
TuanUno <TuanUno@UNOCOMPUTER>
2021-07-12 21:57:20 +0700

    Add project files.

---
+++ AES_Encrypt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AES_Encrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
